5569d46ae3bb2d5ebc7eac164fdb37a4d56911ad
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-05-15 22:26:04 +0100

    show-selector only

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-function getData () {
-  return fetch("https://api.tvmaze.com/shows/82/episodes")
-  .then(async (response) => await response.json())
-  .then(data => data).catch(error => console.log(error))
-}
-// fetch("https://api.tvmaze.com/shows/82/episodes")
-//   .then(async (response) => await response.json())
-//   .then(data => getData(data))
-//   .catch(error => console.log(error))
+let showIdNumber = 82;
+const availableShows = [];
 
-
-// const allEpisodes = getAllEpisodes();
-// const allGotEpisodes = getData();
-const allEpisodes =[];
-getData().then(episode => {
-      episode.forEach(element => allEpisodes.push(element));
+function getAllShows() {
+  return fetch("https://api.tvmaze.com/shows")
+    .then((response) => response.json())
+    .then((data) => {
+      let select = document.querySelector("#show-selector");
+      console.log(data);
+      data.forEach(({name, id}) => {
+        let option = document.createElement("option");
+        option.innerText = name;
+        select.appendChild(option);
+        availableShows.push({name, id});
+      });
     })
+    .catch((error) => console.log(error));
+}
 
-  function setup() {
-    allEpisodes.forEach(element => makeDivForEpisode(element));
+// pull all episodes from the selected show
+function getData() {
+  return fetch(`https://api.tvmaze.com/shows/${showIdNumber}/episodes`)
+    .then(async (response) => await response.json())
+    .then((data) => data)
+    .catch((error) => console.log(error));
+}
+
+const allEpisodes = [];
+getData().then((episode) => {
+  episode.forEach((element) => allEpisodes.push(element));
+});
+
+function setup() {
+  allEpisodes.forEach((element) => makeDivForEpisode(element));
+  getAllShows();
   // const oneEpisode = getOneEpisode();
 }
 
@@ -27,12 +42,14 @@ const episodesDiv = document.querySelector("#episodsHolder");
 
 // function for displaying all episodes
 
-function makeDivForEpisode (episode) {
+function makeDivForEpisode(episode) {
   const divContainer = document.createElement("div");
   divContainer.className = "episodeContainer";
 
   let title = document.createElement("h3");
-  let episodeCode = `S${(String(episode.season).padStart(2,'0'))}E${String(episode.number).padStart(2,'0')}`;
+  let episodeCode = `S${String(episode.season).padStart(2, "0")}E${String(
+    episode.number
+  ).padStart(2, "0")}`;
   title.innerText = `${episode.name} - ${episodeCode}`;
   divContainer.appendChild(title);
 
@@ -45,63 +62,83 @@ function makeDivForEpisode (episode) {
   divContainer.appendChild(description);
 
   episodesDiv.appendChild(divContainer);
-
 }
 
-// search input
+// search for episodes input
 document.querySelector("#search-input").addEventListener("input", searchText);
 
-function searchText(){
-  const searchInput = document.querySelector("#search-input").value.toLowerCase();
-  const filteredEpisodes = allEpisodes.filter(episode => {
-      if (episode.name.toLowerCase().includes(searchInput) || episode.summary.toLowerCase().includes(searchInput)){
-        return episode
-      }
-  })
+function searchText() {
+  const searchInput = document
+    .querySelector("#search-input")
+    .value.toLowerCase();
+  const filteredEpisodes = allEpisodes.filter((episode) => {
+    if (
+      episode.name.toLowerCase().includes(searchInput) ||
+      episode.summary.toLowerCase().includes(searchInput)
+    ) {
+      return episode;
+    }
+  });
   episodesDiv.innerHTML = "";
   document.querySelector("#quantity").innerText = filteredEpisodes.length;
-  filteredEpisodes.forEach(episode => makeDivForEpisode(episode));
+  filteredEpisodes.forEach((episode) => makeDivForEpisode(episode));
 }
 
-// select input option
-function getInputList (originalList) {
-    const selectList = [];
+// select episodes input option
+function getInputList(originalList) {
+  const selectList = [];
 
-    for (let i=0; i < originalList.length; i++){
-      let listItem = {};
-      listItem.name = originalList[i].name;
-      listItem.episodeCode = `S${String(originalList[i].season).padStart(2, "0")}E${String(originalList[i].number).padStart(2, "0")}`;
-      selectList.push(listItem);
-    }
-    return selectList;
+  for (let i = 0; i < originalList.length; i++) {
+    let listItem = {};
+    listItem.name = originalList[i].name;
+    listItem.episodeCode = `S${String(originalList[i].season).padStart(
+      2,
+      "0"
+    )}E${String(originalList[i].number).padStart(2, "0")}`;
+    selectList.push(listItem);
+  }
+  return selectList;
 }
 
-function optionList (){
+function optionList() {
   const listOfOptions = getInputList(allEpisodes);
-  listOfOptions.forEach(item => {
-  let optionValue = document.createElement("option");
-  optionValue.innerText = `${item.episodeCode} - ${item.name}`;
+  listOfOptions.forEach((item) => {
+    let optionValue = document.createElement("option");
+    optionValue.innerText = `${item.episodeCode} - ${item.name}`;
 
-  document.querySelector("#episode-selector").appendChild(optionValue);
-})
+    document.querySelector("#episode-selector").appendChild(optionValue);
+  });
 }
 
 setTimeout(optionList, 2000);
 
 // show chosen episode
-document.querySelector("#episode-selector").addEventListener("change", (event) => {
-  let inputValue = event.target.value.slice(9);
-  let episodeToShow = allEpisodes.find(item => item.name === inputValue);
-  if (episodeToShow === undefined) {
-    setup();
-  } else {
-    episodesDiv.innerHTML = "";
-    document.querySelector("#quantity").innerText = 1;
-    makeDivForEpisode(episodeToShow);
-  }
+document
+  .querySelector("#episode-selector")
+  .addEventListener("change", (event) => {
+    let inputValue = event.target.value.slice(9);
+    let episodeToShow = allEpisodes.find((item) => item.name === inputValue);
+    if (episodeToShow === undefined) {
+      episodesDiv.innerHTML = "";
+      setup();
+    } else {
+      episodesDiv.innerHTML = "";
+      document.querySelector("#quantity").innerText = 1;
+      makeDivForEpisode(episodeToShow);
+    }
+  });
+
+
+// show chosen show
+document.querySelector("#show-selector")
+.addEventListener("change", (event) => {
+  let chosenShow = availableShows.filter(show => show.name === event.target.value);
+  showIdNumber = chosenShow[0].id;
+  console.log(getData());
   
-});
-// 
+  
+})
+//
 // 2.get input selected value
 // 3.match the input value with the name of the episode (indexOf, some, find ....), display this episode
 // 4. clear the select input and get all episodes back
